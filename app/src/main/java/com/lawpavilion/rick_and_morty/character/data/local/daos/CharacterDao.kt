744fe1dbf27f549c9721dfe33b2a6d7ac9a9d7cd
FegzDev <fegzdev@gmail.com>
2026-05-10 10:49:48 +0100

    Add character dao.

---
+++ app/src/main/java/com/lawpavilion/rick_and_morty/character/data/local/daos/CharacterDao.kt
@@ -0,0 +1,21 @@
+package com.lawpavilion.rick_and_morty.character.data.local.daos
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.lawpavilion.rick_and_morty.character.data.local.entities.CharacterEntity
+
+@Dao
+interface CharacterDao {
+    @Query("SELECT * FROM characters ORDER BY id ASC LIMIT :count OFFSET :offset")
+    suspend fun getCharacters(offset: Int, count: Int): List<CharacterEntity>
+
+    @Query("SELECT * FROM characters WHERE id = :id LIMIT 1")
+    suspend fun getCharacter(id: Int): CharacterEntity?
+
+    @Upsert
+    suspend fun upsertCharacters(characters: List<CharacterEntity>)
+
+    @Upsert
+    suspend fun upsertCharacter(character: CharacterEntity)
+}
